14b3f7da30e31f594cf0ef4a38c91e4f9b389762
pseres <32490890+pseres@users.noreply.github.com>
2025-10-06 23:47:31 +0300

    Add files via upload
    Modified the REST API for handling products browsing and added k6 test.

---
+++ Homework/Homework/internal/grpc/server/server.go
@@ -0,0 +1,62 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package main implements a server for Greeter service.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "homework/internal/grpc/proto"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedCartComponentServer
+}
+
+// AddToCart implements the CartComponent AddToCart RPC.
+func (s *server) AddToCart(ctx context.Context, req *pb.CartRequest) (*pb.CartResponse, error) {
+	log.Printf("Received AddToCart request: %+v", req)
+	// For demonstration, just return a dummy cartID.
+	return &pb.CartResponse{CartID: 12345}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterCartComponentServer(s, &server{})
+	log.Printf("gRPC server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
